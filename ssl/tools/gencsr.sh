2e6d1cc86f277b2343fbf1fcefc3c7e8d9a19515
Rakesh Didwania <rakesh.didwania@gmail.com>
2018-09-20 19:49:22 -0500

    Some simple scripts to generate local CA, and some supporting scripts.

---
+++ ssl/tools/gencsr.sh
@@ -0,0 +1,60 @@
+#!/bin/sh
+
+usage() {
+	echo genoutfile.sh
+	echo "    [-c <config-file>]"
+	echo "    [-k <key-file>]"
+	echo "    [-p <key-pass-phrase>]"
+	echo "    [-o <file-name>]"
+	echo
+	echo "Generates Certificate Signing Request. It relies on"
+	echo "the subjectAltName extension to provide multiple host"
+	echo "name for the certificate signing request."
+	echo
+	echo "Sample config:"
+	echo "[ req ]"
+	echo "..."
+	echo "req_extensions = san_ext"
+	echo "..."
+	echo
+	echo "[ san_ext ]"
+	echo "subjectAltName = @alternate_names"
+	echo
+	echo "[ alternate_names ]"
+	echo "DNS.1 = mail.example.com"
+	echo "DNS.2 = dns.example.com"
+	echo "DNS.3 = docs.example.com"
+	echo "..."
+	echo "DNS.n = nth.example.com"
+	echo
+	exit 1
+}
+
+config=
+keyfile=
+passphrase=
+outfile=
+
+while [ $# -gt 0 ]
+do
+	case $1 in
+		-c) config=$2; shift ;;
+		-k) keyfile=$2; shift ;;
+		-o) outfile=$2; shift ;;
+		-p) passphrase="-passin pass:\"$2\""; shift;;
+		*) usage ;;
+	esac
+	shift
+done
+
+ignore=${config:? "config file not specified"}
+ignore=${keyfile:? "key file not specified"}
+ignore=${outfile:? "output file not specified"}
+
+openssl req -new -config ${config} -key ${keyfile} ${passphrase} -outform pem -out ${outfile}
+
+if [ $? -eq 0 ]
+then
+	echo
+	openssl req -text -in ${outfile} -noout
+fi
---
+++ ssl/tools/genkey.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+set -eu
+
+usage() {
+	echo genkey.sh
+	echo "    [-o <key-file>]"
+	echo "    [-p <enc-pass-phrase>]"
+	echo
+	echo Generates RSA key. The key size is 2048 and the public
+	echo exponent is 65537. The key is generated in PEM format
+	echo and is encrypted with AES-256 encryption.
+	echo
+	exit 1
+}
+
+outfile=
+passphrase=
+
+while [ $# -gt 0 ]
+do
+	case $1 in
+		-o) outfile=$2; shift ;;
+		-p) passphrase="-pass pass:\"$2\""; shift;;
+		*) usage ;;
+	esac
+	shift
+done
+
+ignore=${outfile?= "output file not specified"}
+
+openssl genpkey -algorithm RSA -pkeyopt rsa_keygen_bits:4096 -pkeyopt rsa_keygen_pubexp:65537 -aes256 ${passphrase} -outform pem -out ${outfile}
